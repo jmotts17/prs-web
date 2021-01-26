452c1bece7eaaaa8ae61f7057b2cbc4a50a97e8b
Josh Motta <joshua.a.motta@gmail.com>
2021-01-26 16:02:49 -0500

    Updated user reviewer/admin setters

--- src/main/java/com/prs/business/User.java
+++ src/main/java/com/prs/business/User.java
@@ -98,7 +98,7 @@ public class User {
 		return isReviewer;
 	}
 
-	public void setIsReviewer(boolean isReviewer) {
+	public void setReviewer(boolean isReviewer) {
 		this.isReviewer = isReviewer;
 	}
 
@@ -106,7 +106,7 @@ public class User {
 		return isAdmin;
 	}
 
-	public void setIsAdmin(boolean isAdmin) {
+	public void setAdmin(boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
 	
